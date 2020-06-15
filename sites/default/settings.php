7cad211d2d6eb12e6aa4ec1710dccd80c2b86a99
EZunigaF <estebanzfl@gmail.com>
2020-06-15 03:29:31 -0600

    Portfolio completed entregado

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,6 +1,16 @@
 <?php
 
+/ Relative path.
+$settings['default_content_deploy_content_directory'] = '../content';
+// Absolute path.
+$settings['default_content_deploy_content_directory'] = '/var/dcd/content';
+
+global $content_directories;
+$content_directories['sync'] = $app_root.'/../content/sync';
+
 // #ddev-generated: Automatically generated Drupal settings file.
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
+
 }
+
